3a88b7b82a6ef8a648a96743fb5a61165b13ecc6
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-17 15:27:48 -0300

    Class for getting data with a MAP

---
+++ lib/models/question.dart
@@ -0,0 +1,30 @@
+class Question {
+  String question;
+  String answer1;
+  String answer2;
+
+  Question({
+    this.question,
+    this.answer1,
+    this.answer2,
+});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'question' : question,
+      'answer1' : answer1,
+      'answer2' : answer2,
+    };
+  }
+
+ static Question fromMap(Map<String, dynamic> map) {
+    if (map = null) return null;
+
+    return Question(
+      question: map['question'],
+      answer1: map['answer1'],
+      answer2: map['answer2'],
+    );
+ } 
+
+}
