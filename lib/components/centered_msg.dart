94bd7153c90555b69bd27ccd83f623695e60ee65
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-18 13:13:57 -0300

    centered message with question

--- lib/components/centered_msg.dart
+++ lib/components/centered_msg.dart
@@ -17,7 +17,31 @@ class CenteredMsg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Visibility(
+            child: Icon(
+              icon,
+              size: iconSize,
+              color: Colors.black26,
+            ),
+            visible: icon != null,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 24.0),
+            child: Text(
+              message,
+              style: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.w500,
+                color: Colors.black26,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
